335c503d67d8eb6832fd7e3df92f93998c4d9e39
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-06 16:35:28 +0100

    add : js-function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const bottone = document.getElementById('button')
 const lightOff = document.getElementById('spenta')
 const lightOn = document.getElementById('accesa')
+
+bottone.addEventListener('click', function() {
+  lightOff.classList.toggle('active')
+  lightOn.classList.toggle('active')
+  bottone.innerHTML = lightOff.classList.contains('active') ? 'Spegni' : 'Accendi';
+});
